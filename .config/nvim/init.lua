54f9cd05b98f4a8218ca2628ee39861756aaf645
Kike <enrodpre@gmail.com>
2026-03-14 08:14:11 +0100

    backup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,41 +2,8 @@ require("options")
 require("functions")
 require("config").setup()
 require("library.lang")
-
-local lazy_installed = vim.uv.fs_stat(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
-if not lazy_installed then
-  load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"))()
-end
-
-require("lazy").setup({ { import = "plugins", }, }, {
-  dev = {
-    path = "~/dev/nvim/plugins",
-    -- patterns = { "themery.nvim", "cmake-tools.nvim" },
-    fallback = true
-  },
-  defaults = { lazy = true, },
-  rocks = { root = "/home/kike/.local/lib/luarocks/rocks-5.1", },
-  change_detection = {
-    enabled = true,
-    notify = false,
-  },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "netrwPlugin",
-        "rpmPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
-  profiling = { loader = true, require = true, },
-})
-
+require("bootstrap")
 require("library")
 
 -- vim.packadd()
-vim.cmd [[packadd nohlsearch]]
+vim.cmd([[packadd nohlsearch]])
--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -33,10 +33,8 @@ export TERM_EMULATOR=/usr/bin/kitty
 export PYTHON_HISTORY="$XDG_STATE_HOME/python_history"
 export PYTHONPATH=$XDG_DATA_HOME/python/functions:$HOME/.local/bin:/bin/python
 
-export LUA_PATH='/usr/share/lua/5.1/?.lua;./?.lua;./?/init.lua;'
-export LUA_CPATH='$HOME/.local/lib/lua/5.1/?.so;/usr/lib/lua/5.1/?.so;./?.so;$HOME/.local/lib/lua/5.1/?.so'
-
-echo "$path"
+#export LUA_PATH='/usr/share/lua/5.1/?.lua;./?.lua;./?/init.lua;'
+#export LUA_CPATH='$HOME/.local/lib/lua/5.1/?.so;/usr/lib/lua/5.1/?.so;./?.so;$HOME/.local/lib/lua/5.1/?.so'
 
 path=(
   "$HOME"/.local/bin
